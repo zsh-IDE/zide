0daa9e5d9b5733f08a86d0f445da08dd47fba386
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2019-11-25 13:35:40 +0100

    Initial commit

---
+++ zide.plugin.zsh
@@ -0,0 +1,14 @@
+# According to the Zsh Plugin Standard:
+# http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+
+0=${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}
+0=${${(M)0:#/*}:-$PWD/$0}
+
+# Then ${0:h} to get plugin's directory
+
+if [[ ${zsh_loaded_plugins[-1]} != */zide && -z ${fpath[(r)${0:h}]} ]]
+then
+    fpath+=( "${0:h}" )
+fi
+
+autoload -Uz zide
