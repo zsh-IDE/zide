c4b9adcf231673be9e8526014031dfb98a016bca
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2019-12-03 10:51:11 +0100

    templates/script.tmpl.zsh

---
+++ templates/script.tmpl.zsh
@@ -0,0 +1,24 @@
+# -*- mode: sh; sh-indentation: $shift; sh-basic-offset: $shift;$emacstabs -*-
+# vim:ft=zsh:sw=$shift:sts=$shift$vimtabs:
+
+# Copyright $year $user_name 
+# License: $license
+
+# Run as script? ZSH_SCRIPT is a Zsh 5.3 addition
+if [[ $0 != "$script_name" || -n $ZSH_SCRIPT ]]; then
+    # Handle $0 according to the Zsh Plugin Standard:
+    # http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+    0=${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}
+    0=${${(M)0##/*}:-$PWD/$0}
+else
+    0=${funcsourcetrace[-1]%:*}
+fi
+
+emulate -LR zsh
+setopt extendedglob warncreateglobal typesetsilent noshortloops rcquotes
+    
+typeset $script_name_DIR=${0:h}
+fpath+=( $$script_name_DIR )
+path+=( $$script_name_DIR )
+
+# Load/setup the components
